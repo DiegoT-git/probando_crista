b46279d5062745d252ae2a4846d36c1e9f077cdb
Diego Torres <ditocapy@gmail.com>
2024-08-14 09:38:48 -0400

    colocando la funcion de suma

--- multiplicando.py
+++ multiplicando.py
@@ -3,8 +3,15 @@ def multiplicar(a,b):
     print("la multiplicacion es:")
     return a * b
 
+def suma(a,b):
+    #hecho por diego
+    print("la suma entre los numeros es: ")
+    print("que locura")
+    return a + b
+
 
 print(multiplicar(3,4))
+print(suma(3,7))
 
 
 
