2fd719e7ffc9542e8c4c9631535f8d6d0a8ac1cc
Diego Torres <ditocapy@gmail.com>
2024-08-14 09:25:35 -0400

    esta es la funcion de multiplicacion

---
+++ multiplicando.py
@@ -0,0 +1,15 @@
+def multiplicar(a,b):
+    print("la multi es:")
+    print("suma sucesiva de numeros")
+    print("la suma en este caso es")
+    return a * b
+
+
+print(multiplicar(3,4))
+
+
+
+
+
+
+
